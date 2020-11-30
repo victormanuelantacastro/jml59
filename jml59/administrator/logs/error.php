8cfce35f53700ef77ae1e1308f7a7cbe536c18a3
Victor <anta_castro_victor_manuel@iescamas.es>
2020-11-30 11:49:38 +0100

    Commit examen subir carpeta desde cdmon

---
+++ jml59/administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-11-30 10:22:17 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-11-30T10:22:17+00:00	INFO 62.83.70.4	joomlafailure	Username and password do not match or you do not have an account yet.
